8529792d7fc928252a3811c4edb83b98e8c1a40f
Deivinson Tejeda <deivinsontejeda@gmail.com>
2014-06-08 21:16:50 -0400

    rewrite firt implementation.
      - Now using channel to real concurrency scheme

--- gapi/concurrency.go
+++ gapi/concurrency.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 const (
@@ -17,45 +18,57 @@ type UserResponse struct {
 	Name  string `json."name"`
 }
 
-type ErrorResponse struct {
-	message           string `json:"message"`
-	documentation_url string `json:"documentation_url"`
-}
-
-func GetProfile(username string, i interface{}) (e error) {
-	url := fmt.Sprintf("%s/%s", GITHUB_ENDPOINT, username)
-	resp, err := http.Get(url)
-
-	if err != nil {
-		return err
-	}
+// Perform a call to GITHUB API and parser the response
+func GetJSON(u string) UserResponse {
+	url := fmt.Sprintf("%s/%s", GITHUB_ENDPOINT, u)
+	resp, _ := http.Get(url)
 
 	defer resp.Body.Close()
 
 	b, e := ioutil.ReadAll(resp.Body)
 
-	//does not work :/
-	if resp.StatusCode != 200 {
-		gr := &ErrorResponse{}
-		json.Unmarshal(b, gr)
-		fmt.Println(gr)
-		return fmt.Errorf(gr.message)
-	}
 	if e != nil {
 		fmt.Println("Error2", resp.Body)
-		return e
 	}
+	i := &UserResponse{}
 	e = json.Unmarshal(b, i)
-	if e != nil {
-		fmt.Println("Something wrong bad :(")
-		return e
+	return *i
+
+}
+
+func GetProfile(users <-chan string, ur chan<- UserResponse) {
+	for u := range users {
+		ur <- GetJSON(u)
 	}
-	return
 }
 
 func main() {
-	username := "deivinsontejeda"
-	ur := &UserResponse{}
-	GetProfile(username, ur)
-	fmt.Println("-> ", ur)
+	// figure out how works size
+	usersChan := make(chan string, 4)
+	ur := make(chan UserResponse, 4)
+
+	// taken args each one is a user
+	args := os.Args[1:]
+
+	// spanw up to  3 workers
+	for w := 1; w <= 3; w++ {
+		go GetProfile(usersChan, ur)
+	}
+
+	// Intereting:
+	// Workers are blocking until we did not assign job to processing
+	// in this case the `jobs` are store in `usersChan` channel
+
+	//Send how many `job` as user we collected from CLI
+	for _, u := range args {
+		usersChan <- u
+	}
+
+	defer close(usersChan)
+
+	// Collect all the results of work
+	for f := 1; f <= len(args); f++ {
+		fmt.Println(<-ur)
+	}
+
 }
